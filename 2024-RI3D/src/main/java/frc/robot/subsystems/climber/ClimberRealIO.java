7f6ddde8ca9943a056a71593f0368d85518e3ef3
QuoteDev <31429539+QuoteDev@users.noreply.github.com>
2024-01-06 22:55:42 -0700

    before testing

--- 2024-RI3D/src/main/java/frc/robot/subsystems/climber/ClimberRealIO.java
+++ 2024-RI3D/src/main/java/frc/robot/subsystems/climber/ClimberRealIO.java
@@ -7,6 +7,9 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import static frc.robot.Constants.*;
 
+import java.lang.annotation.Target;
+import java.lang.reflect.GenericSignatureFormatError;
+
 public class ClimberRealIO implements ClimberIO {
 
     private CANSparkMax climberMotor;
@@ -15,11 +18,11 @@ public class ClimberRealIO implements ClimberIO {
     private RelativeEncoder climberMotorEncoder;
     private RelativeEncoder climberWenchMotorEncoder;
 
-    private SparkMaxPIDController m_pidController;
+    private SparkMaxPIDController wenchPIDController;
 
     private boolean isBrake;
 
-    public double setPoint;
+    public double targetPos;
 
     public double kP, kI, kD, kMaxOutput, kMinOutput, maxVel, minVel, maxAcc, allowedErr;
 
@@ -40,7 +43,7 @@ public class ClimberRealIO implements ClimberIO {
         climberMotor.setInverted(true);
         climberWenchMotor.setInverted(true);
 
-        m_pidController = climberMotor.getPIDController();
+        wenchPIDController = climberMotor.getPIDController();
         climberMotorEncoder = climberMotor.getEncoder();
         climberWenchMotorEncoder = climberWenchMotor.getEncoder();
 
@@ -51,10 +54,10 @@ public class ClimberRealIO implements ClimberIO {
         kMaxOutput = 1; 
         kMinOutput = -1;
 
-        m_pidController.setP(kP);
-        m_pidController.setI(kI);
-        m_pidController.setD(kD);
-        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+        wenchPIDController.setP(kP);
+        wenchPIDController.setI(kI);
+        wenchPIDController.setD(kD);
+        wenchPIDController.setOutputRange(kMinOutput, kMaxOutput);
 
         climberMotorEncoder.setPositionConversionFactor(CLIMB_RADIO);
         climberWenchMotorEncoder.setPositionConversionFactor(CLIMB_RADIO);
@@ -71,24 +74,21 @@ public class ClimberRealIO implements ClimberIO {
         inputs.climberCurrentWench = climberWenchMotor.getOutputCurrent();
         inputs.appliedPower = climberMotor.getAppliedOutput();
         inputs.appliedPowerWench = climberWenchMotor.getAppliedOutput();
-        inputs.targetPos = 0;
+        inputs.targetPos = this.targetPos;
         inputs.angularPos = 0;
         inputs.angularPosWench = 0;
 
     }
 
-    public void updateSetPoint() {
-        m_pidController.setReference(setPoint, CANSparkMax.ControlType.kSmartMotion);
+    public void updateSetPoint(double setPoint) {
+        wenchPIDController.setReference(setPoint, CANSparkMax.ControlType.kPosition);
+        targetPos = setPoint;
     }
 
     public void setWenchPower(double power) {
         climberWenchMotor.set(power);
       }
-    
-    public void setBarHeight(double height) {
-        final double currentHeight = climberMotorEncoder.getPosition();
 
-    }
     public void resetClimberEncoder(){
       climberMotorEncoder.setPosition(0);
     }
