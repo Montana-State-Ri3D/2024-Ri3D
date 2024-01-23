ef779b01ecc4c259167d205c2e42ca50173b267b
QuoteDev <31429539+QuoteDev@users.noreply.github.com>
2024-01-22 20:10:31 -0700

    autoShootDrive and RumbleCommand completed

---
+++ src/main/java/frc/robot/commands/RumbleCommand.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RumbleCommand extends Command {
+  private int Power;
+  private final XboxController HID = new XboxController(0);
+
+/** Creates a new DriveCommand. */
+
+  public RumbleCommand(int Power) {
+    this.Power = Power;
+
+    HID.setRumble(RumbleType.kLeftRumble, 1.0);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    HID.setRumble(RumbleType.kLeftRumble, 0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
