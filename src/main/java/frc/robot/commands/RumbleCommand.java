1c1c7cfd3a13e0cc18045f7368bdb8707418bac0
Joshua Elmore <boyscout574@outlook.com>
2024-01-29 22:27:44 -0700

    removed bin

--- src/main/java/frc/robot/commands/RumbleCommand.java
+++ src/main/java/frc/robot/commands/RumbleCommand.java
@@ -2,19 +2,16 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RumbleCommand extends Command {
-  private int Power;
   private final XboxController HID = new XboxController(0);
 
-/** Creates a new DriveCommand. */
-
   public RumbleCommand(int Power) {
-    this.Power = Power;
 
-    HID.setRumble(RumbleType.kLeftRumble, Power);
+    HID.setRumble(RumbleType.kBothRumble, Power);
+
+
 
   }
 
@@ -31,7 +28,7 @@ public class RumbleCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    HID.setRumble(RumbleType.kLeftRumble, 0.0);
+    HID.setRumble(RumbleType.kBothRumble, 0.0);
   }
 
   // Returns true when the command should end.
